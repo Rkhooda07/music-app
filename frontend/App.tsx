d433247bdb2851faceea0338e8fde0e136d76b5f
Rakshit Hooda <rakshit.official703@gmail.com>
2025-12-14 00:59:43 +0530

    feat(frontend): setup bottom tab navigation and placeholder screens

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./src/screens/HomeScreen";
+import SearchScreen from "./src/screens/SearchScreen";
+import LibraryScreen from "./src/screens/LibraryScreen";
+import PlayerScreen from "./src/screens/PlayerScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>I know nothin' about react native lol</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name= "Home" component={HomeScreen} />
+        <Tab.Screen name= "Search" component={SearchScreen} />
+        <Tab.Screen name= "Library" component={LibraryScreen} />
+        <Tab.Screen name= "Player" component={PlayerScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
